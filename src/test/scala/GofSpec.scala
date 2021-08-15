7b5f30bcc44ceda8d6ee87541234bcb75963b5fa
gaurav <gaurav.k.89@gmail.com>
2021-08-15 17:46:55 +0530

    Game of life impl and spec

--- src/test/scala/GofSpec.scala
+++ src/test/scala/GofSpec.scala
@@ -5,20 +5,20 @@ class GofSpec extends AnyFunSuite {
   test("Scenario: Compute next state of grid") {
 
     val ob = new Gof(5,7)
-    ob.setSeedState(Array((1,3),(1,4),(2,4)))
+    ob.setSeedState(Array((1,3),(2,4),(3,2),(3,3),(3,4)))
     assert(isEqual(ob.getGrid,Array(
       Array(0,0,0,0,0,0,0),
-      Array(0,0,0,1,1,0,0),
+      Array(0,0,0,1,0,0,0),
       Array(0,0,0,0,1,0,0),
-      Array(0,0,0,0,0,0,0),
+      Array(0,0,1,1,1,0,0),
       Array(0,0,0,0,0,0,0)
     )),"Seed state is incorrectly set")
     ob.computeNextNthState()
     assert(isEqual(ob.getGrid,Array(
       Array(0,0,0,0,0,0,0),
-      Array(0,0,0,1,1,0,0),
-      Array(0,0,0,1,1,0,0),
       Array(0,0,0,0,0,0,0),
+      Array(0,0,1,0,1,0,0),
+      Array(0,0,0,1,1,0,0),
       Array(0,0,0,0,0,0,0)
     )),"Next state is computed incorrectly")
   }
